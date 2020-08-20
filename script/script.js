06932a6a12a4847a6a0dbbc0ad74cbf2e3d4e163
Adam <sinegamy11@gmail.com>
2020-08-21 00:06:29 +0700

    add task2

---
+++ script/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const listShow = () => {
+	const clubsSelect = document.querySelector('.club-select'),
+			list = clubsSelect.querySelector('ul');
+
+	clubsSelect.addEventListener('click', () => {
+		!list.style.display ? 
+		list.style.display = 'inline-block' :
+		list.style.display = '';
+	});
+}
+
+document.addEventListener('DOMContentLoaded', listShow);
