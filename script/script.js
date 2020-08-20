da6ab538f64866fd6e11a6cb4bcb7da7ce78a5bb
Adam <sinegamy11@gmail.com>
2020-08-21 00:23:43 +0700

    add popup1

--- script/script.js
+++ script/script.js
@@ -11,4 +11,21 @@ const listShow = () => {
 	});
 }
 
-document.addEventListener('DOMContentLoaded', listShow);
+const popup = () => {
+	const openPopup = document.querySelector('.open-popup'),
+			popup = document.getElementById('free_visit_form');
+	
+	openPopup.addEventListener('click', () => {
+		popup.style.display = 'block';
+	});
+
+	popup.addEventListener('click', (event) => {
+		const target = event.target;
+		if(target.classList.contains('close_icon') || !target.closest('.form-content')) {
+			popup.style.display = '';
+		};
+	});
+}
+
+listShow();
+popup();
