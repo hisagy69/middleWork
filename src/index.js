564b8a69b39b93aadba0067053fc290c07ee6d80
Adam <sinegamy11@gmail.com>
2020-08-22 00:37:29 +0700

    add scroll arrow

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@
 import listShow from './modules/listShow';
 import popup from './modules/popup';
 import './modules/burger';
+import arrowTop from './modules/arrowTop';
 
 
 listShow();
 popup('.open-popup', 'free_visit_form');
 popup('.fixed-gift', 'gift', true);
+popup('.callback-btn', 'callback_form');
+arrowTop('clubs', 'totop')
 // burger();
