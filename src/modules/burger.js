07633f576834814c14bd46270ff88cf142c90b3f
Adam <sinegamy11@gmail.com>
2020-08-21 18:32:01 +0700

    add scrol hide show burger and width innerwidth

--- src/modules/burger.js
+++ src/modules/burger.js
@@ -1,15 +1,30 @@
 //burger
 export default (() => {
 	const menuButton = document.querySelector('.menu-button'),
-			menu = document.querySelector('.popup-menu');
+		menu = document.querySelector('.popup-menu'),
+		topMenu = document.querySelector('.top-menu');
+	if (innerWidth <= 768) {
+		menuButton.style.display = 'block';
 
-	menuButton.addEventListener('click', () => {
-		menu.style.display ='flex';
-	});
-	menu.addEventListener('click', (event) => {
-		const target = event.target;
-		if (target.matches('.close-menu-btn img') || target.closest('.scroll')) {
-			menu.style.display = '';
-		}
-	});
+		menuButton.addEventListener('click', () => {
+			menu.style.display ='flex';
+		});
+		menu.addEventListener('click', (event) => {
+			const target = event.target;
+			if (target.matches('.close-menu-btn img') || target.closest('.scroll')) {
+				menu.style.display = '';
+			}
+		});
+		window.addEventListener('scroll', () => {
+			if (topMenu.getBoundingClientRect().top === 0){
+				topMenu.style.cssText = '';
+			}
+			if (topMenu.getBoundingClientRect().top < 0) {
+				topMenu.style.cssText = `
+					position: fixed;
+					top: 0;
+				`;
+			}
+		});
+	}
 })();
