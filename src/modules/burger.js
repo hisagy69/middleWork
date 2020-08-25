0a7bbfe2b02b2963f27df08fe3808b4517127f02
Adam <sinegamy11@gmail.com>
2020-08-25 12:12:33 +0700

    fix menu

--- src/modules/burger.js
+++ src/modules/burger.js
@@ -3,35 +3,40 @@ export default (() => {
 	const menuButton = document.querySelector('.menu-button'),
 		menu = document.querySelector('.popup-menu'),
 		topMenu = document.querySelector('.top-menu');
-	if (innerWidth <= 768) {
-		menuButton.style.display = 'block';
-
-		menuButton.addEventListener('click', () => {
-			menu.style.display ='flex';
-		});
-		menu.addEventListener('click', (event) => {
-			const target = event.target;
-			if (target.matches('.close-menu-btn img') || target.closest('.scroll')) {
-				menu.style.display = '';
-			}
-		});
-		window.addEventListener('scroll', () => {
-			if (topMenu.getBoundingClientRect().top === 0){
-				topMenu.style.cssText = '';
-			}
-			if (topMenu.getBoundingClientRect().top < 0 && innerWidth < 768) {
-				topMenu.style.cssText = `
-					position: fixed;
-					top: 0;
-				`;
-			}
-		});
-		window.addEventListener('resize', () => {
-			if (innerWidth > 768) {
-				menuButton.style.display = '';
-				menu.style.display = '';
-				topMenu.style.cssText = '';
-			}
-		});
+	const menuShow = () => {
+		if (innerWidth <= 768) {
+			menuButton.style.display = 'block';
+	
+			menuButton.addEventListener('click', () => {
+				menu.style.display ='flex';
+			});
+			menu.addEventListener('click', (event) => {
+				const target = event.target;
+				if (target.matches('.close-menu-btn img') || target.closest('.scroll')) {
+					menu.style.display = '';
+				}
+			});
+			window.addEventListener('scroll', () => {
+				if (topMenu.getBoundingClientRect().top === 0){
+					topMenu.style.cssText = '';
+				}
+				if (topMenu.getBoundingClientRect().top < 0 && innerWidth < 768) {
+					topMenu.style.cssText = `
+						position: fixed;
+						top: 0;
+					`;
+				}
+			});
+		}
 	}
+	window.addEventListener('resize', () => {
+		if (innerWidth > 768) {
+			menuButton.style.display = '';
+			menu.style.display = '';
+			topMenu.style.cssText = '';
+		} else {
+			menuShow();
+		}
+	});
+	menuShow();
 })();
