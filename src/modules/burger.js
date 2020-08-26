2da5d5d4687685a6409b55c771f57a5198023cb9
Adam <sinegamy11@gmail.com>
2020-08-26 19:36:00 +0700

    fix burger

--- src/modules/burger.js
+++ src/modules/burger.js
@@ -3,30 +3,29 @@ const burger = () => {
 		const menuButton = document.querySelector('.menu-button'),
 		menu = document.querySelector('.popup-menu'),
 		topMenu = document.querySelector('.top-menu');
+		let position = topMenu.getBoundingClientRect().top;
 		const menuShow = () => {
 			if (innerWidth <= 768) {
 				menuButton.style.display = 'block';
 		
 				menuButton.addEventListener('click', () => {
-					menu.style.display ='flex';
+					menu.style.display = 'flex';
 				});
-				menu.addEventListener('click', (event) => {
+				menu.addEventListener('click', event => {
 					const target = event.target;
 					if (target.matches('.close-menu-btn img') || target.closest('.scroll')) {
 						menu.style.display = '';
 					}
 				});
 				window.addEventListener('scroll', () => {
-					const menuScroll = topMenu.getBoundingClientRect().top;
-					console.log(menuScroll);
-					if (menuScroll === 0) {
-						topMenu.style.cssText = '';
-					}
-					if (menuScroll < 0 && innerWidth < 768) {
+					if (document.documentElement.scrollTop > position && innerWidth < 768) {
 						topMenu.style.cssText = `
 							position: fixed;
 							top: 0;
 						`;
+					}
+					if (document.documentElement.scrollTop <= position) {
+						topMenu.style.cssText = '';
 					}
 				});
 			}
